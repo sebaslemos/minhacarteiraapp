6d91ba80ed79c25a49902c449512664cb0dbc89c
slsousa <slsousa@trt13.jus.br>
2018-11-13 13:53:12 -0300

    Corrige o Bug da issue #16

--- app/src/main/java/br/com/sbsistemas/minhacarteira/helpers/FormularioReceitaHelper.java
+++ app/src/main/java/br/com/sbsistemas/minhacarteira/helpers/FormularioReceitaHelper.java
@@ -10,6 +10,7 @@ import java.math.BigDecimal;
 
 import br.com.sbsistemas.minhacarteira.FormularioReceiraActivity;
 import br.com.sbsistemas.minhacarteira.R;
+import br.com.sbsistemas.minhacarteira.exception.MinhaCarteiraInputException;
 import br.com.sbsistemas.minhacarteira.modelo.Receita;
 
 /**
@@ -28,7 +29,12 @@ public class FormularioReceitaHelper {
         data = (TextView) activity.findViewById(R.id.formulario_receita_data);
     }
 
-    public Receita pegaReceitaDoFormulario(){
+    public Receita pegaReceitaDoFormulario() throws MinhaCarteiraInputException {
+        if(isNullOuVazio(descricao.getText().toString(),
+                valor.getText().toString(),
+                data.getText().toString()))
+            throw new MinhaCarteiraInputException("Todos os campos devem ser preenchidos.");
+
         Receita receita = new Receita();
         receita.setDescricao(descricao.getText().toString());
         receita.setValor(new BigDecimal(valor.getText().toString()));
@@ -38,6 +44,14 @@ public class FormularioReceitaHelper {
                 DateTimeFormat.forPattern("dd/MM/yyyy")));
 
         return receita;
+    }
+
+    private boolean isNullOuVazio(String... Stringargs){
+
+        for(String arg : Stringargs)
+            if(arg == null || arg.equals("")) return true;
+
+        return false;
     }
 
 }

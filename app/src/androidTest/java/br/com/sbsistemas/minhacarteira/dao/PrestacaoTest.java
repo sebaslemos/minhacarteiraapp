89af15dfddee3e80f0e080a68773824e1674e999
slsousa <slsousa@trt13.jus.br>
2018-11-29 16:18:29 -0300

    corrige teste

--- app/src/androidTest/java/br/com/sbsistemas/minhacarteira/dao/PrestacaoTest.java
+++ app/src/androidTest/java/br/com/sbsistemas/minhacarteira/dao/PrestacaoTest.java
@@ -3,8 +3,6 @@ package br.com.sbsistemas.minhacarteira.dao;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
 
-import junit.framework.Assert;
-
 import org.joda.time.LocalDate;
 import org.junit.After;
 import org.junit.Before;

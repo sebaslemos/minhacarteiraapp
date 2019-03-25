af92961c45ee8ecfe37348660ac3051a669291ad
slsousa <slsousa@trt13.jus.br>
2019-03-25 11:27:06 -0300

    Corrige erros do projeto que o fazia não executar o build

---
+++ app/src/main/java/br/com/sbsistemas/minhacarteira/BaseActivity.java
@@ -0,0 +1,39 @@
+package br.com.sbsistemas.minhacarteira;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import org.joda.time.LocalDate;
+
+public abstract class BaseActivity {
+//
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        getMenuInflater().inflate(R.menu.menu_lista_grupo, menu);
+//        return super.onCreateOptionsMenu(menu);
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        Intent intent;
+//        switch (item.getItemId()){
+//            case R.id.menu_lista_grupo_add_receita:
+//                intent = new Intent(this, ListaReceitasActivit.class);
+//                intent.putExtra("data", getDataSelecionada());
+//                startActivity(intent);
+//                break;
+//            case R.id.menu_lista_grupo_add_conta:
+//                intent = new Intent(this, FormularioContaActivity.class);
+//                startActivity(intent);
+//                break;
+//            default:
+//                return true;
+//        }
+//        return true;
+//    }
+//
+//    public abstract LocalDate getDataSelecionada();
+
+}
